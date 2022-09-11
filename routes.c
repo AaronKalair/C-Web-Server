b56931a29fa6c23d57448c1202356a66f0b7e109
Daniel Zapata <djz24@case.edu>
2022-09-11 11:06:27 -0500

    comment

--- routes.c
+++ routes.c
@@ -3,6 +3,11 @@ typedef struct {
   char *routename;
 } route;
 
+// TO ADD A ROUTE:
+// 1. define a function that returns a char* here (that is the json content)
+// 2. add a route declaration at the bottom
+//    like: `route stateR = { .routeFnPtr = &state, .routename = "/state.json"};`
+
 char* state(int a)
 {
     return "{\"status\":1}";
